8b0df80ffc85b970b6ed8a112ddb33e5274cd63c
gotodoweb <gotodo@internet.ru>
2021-11-23 01:47:57 +0500

    second task

--- LESSON2/index.js
+++ LESSON2/index.js
@@ -1,1 +1,22 @@
-'use strict';
+'use strict'
+
+{
+	const item = 'smart проектор philips';
+	const count = 15;
+	const category = 'видео техника';
+	const price = 1200;
+
+	console.log(item);
+	console.log(`общая сумма товара: ${count * price}`);
+}
+
+{
+	const item2 = 'sumsung Galaxy S21';
+	const count2 =  10;
+	const category2 = 'mobile phone';
+	const price2 = 64990;
+
+	console.log(item2);
+	console.log(`общая сумма товара: ${count2 * price2}`);
+}
+
