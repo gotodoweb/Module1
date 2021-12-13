5f5144400b94965714dd19a7eacc95900ad4b7c8
gotodoweb <gotodo@internet.ru>
2021-12-13 21:36:04 +0500

    ML1L11hw

--- LESSON10/task1.js
+++ LESSON10/task1.js
@@ -1,46 +1,39 @@
 'use strict';
 
 const cart = {
-	items: [],
-	get totalPrice() {
-		return this.calculateItemPrice();
-	},
-	count: 0,
-	// getTotalPrice() {
-	// 	return this.totalPrice;
-	// },
-	add(item, price, count) {
-		const good = { item, price, count };
-		this.items.push(good);
+  items: [],
+  get totalPrice() {
+    return this.calculateItemPrice();
+  },
+  count: 0,
+  add(item, price, count) {
+    const good = {item, price, count};
+    this.items.push(good);
 
-		this.calculateItemPrice();
-		this.increaseCount(count);
-	},
-	increaseCount(count) {
-		this.count += count;
-	},
-	calculateItemPrice() {
-		let itemPrice = 0;
-		this.items.map((item) => {
-			// console.log(`Добавлено в коризну:товар ${item.item}	общая сумма ${item.price * item.count}`);
-			itemPrice += item.price * item.count;
-
-		});
-	
-		console.log(itemPrice);
-		return itemPrice;
-	},
-	clear() {
-		this.items = [];
-		this.totalPrice = 0;
-		this.count = 0;
-	},
-	print() {
-		const objStr = JSON.stringify(this.items);
-		console.log(objStr);
-		console.log('Общая стоимомость корзины:', cart.totalPrice);
-
-	},
+    this.calculateItemPrice();
+    this.increaseCount(count);
+  },
+  increaseCount(count) {
+    this.count += count;
+  },
+  calculateItemPrice() {
+    let itemPrice = 0;
+    this.items.map((item) => {
+      itemPrice += item.price * item.count;
+    });
+    console.log(itemPrice);
+    return itemPrice;
+  },
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+  print() {
+    const objStr = JSON.stringify(this.items);
+    console.log(objStr);
+    console.log('Общая стоимомость корзины:', cart.totalPrice);
+  },
 };
 
 cart.add('pepsi', 10, 20);
